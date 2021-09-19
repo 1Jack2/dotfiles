1ed6db8316839557e979905d3588b580613585bc
Jack Zhang <jack333zhang@gmail.com>
2021-09-19 22:59:57 +0800

    Migrate to bash-it

---
+++ dot_bash_it/custom/executable_custom.bash
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+#
+# My custom .bashrc
+
+export VISUAL=vi
+export EDITOR=vim
+
+alias cp="cp -i"
+alias rm="rm -i"
+alias mv='mv -i'
+alias gs='git status'
+alias gb='git branch'
+alias fd='fdfind'
+alias proxy='proxychains4'
+alias vi='nvim'
+alias cm='chezmoi'
+
+# https://github.com/trapd00r/LS_COLORS
+. "/home/jack/.local/share/lscolors.sh"
+
+# https://github.com/rupa/z
+# --------------------------------------------------------------------
+. ~/.z_jump/z.sh
+
+# proxychains-ng
+# --------------------------------------------------------------------
+export PROXYCHAINS_CONF_FILE=~/.proxychains.conf
+
+# starship
+# --------------------------------------------------------------------
+eval "$(starship init bash)"
+
