f78bc19018793910e145502a137507a11052e571
Jack Zhang <jack333zhang@gmail.com>
2021-09-20 18:11:19 +0800

    x

--- dot_bash_it/custom/executable_custom.bash
+++ dot_bash_it/custom/executable_custom.bash
@@ -2,31 +2,34 @@
 #
 # My custom .bashrc
 
+export PATH=$PATH:$HOME/.cargo/bin
+export PATH=$PATH:/usr/local/go/bin
+
 export VISUAL=nvim
 export EDITOR=nvim
 
+alias vi='nvim'
 alias cp="cp -i"
 alias rm="rm -i"
 alias mv='mv -i'
 alias gs='git status'
 alias gb='git branch'
-alias fd='fdfind'
 alias proxy='proxychains4'
-alias vi='nvim'
 alias cm='chezmoi'
 
 # https://github.com/trapd00r/LS_COLORS
 . "/home/jack/.local/share/lscolors.sh"
 
 # https://github.com/rupa/z
-# --------------------------------------------------------------------
 . ~/.z_jump/z.sh
 
 # proxychains-ng
-# --------------------------------------------------------------------
 export PROXYCHAINS_CONF_FILE=~/.proxychains.conf
 
 # starship
-# --------------------------------------------------------------------
+export BASH_IT_THEME=''
 eval "$(starship init bash)"
+
+# fzf
+[ -f ~/.fzf.bash ] && source ~/.fzf.bash
 
