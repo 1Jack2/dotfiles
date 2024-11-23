17c883c5fb95289533f2a660bd04a18a04d672aa
jack zhang <jack333zhang@gmail.com>
2024-11-23 16:52:37 +0800

    [fish] add fish shell

---
+++ private_dot_config/private_fish/config.fish
@@ -0,0 +1,99 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+
+    # ======================================== env
+    set -U fish_greeting
+    set --export EDITOR nvim
+
+    # ======================================== tool
+
+    # https://github.com/jdx/mise
+    if command -v mise > /dev/null
+        mise activate fish | source
+    end
+
+    # https://github.com/starship/starship
+    if command -v starship > /dev/null
+        starship init fish | source
+    end
+
+    # https://github.com/ajeetdsouza/zoxide
+    if command -v zoxide > /dev/null
+        zoxide init fish | source
+    end
+
+    # https://github.com/atuinsh/atuin
+    if command -v atuin > /dev/null
+        atuin init fish --disable-up-arrow | source
+    end
+
+    # https://github.com/sharkdp/vivid
+    if command -v vivid > /dev/null
+        set --export LS_COLORS "$(vivid generate gruvbox-light)"
+    end
+
+    # ======================================== alias
+    abbr -a vi 'nvim'
+
+    abbr -a cp "cp -i"
+    abbr -a rm "rm -i"
+    abbr -a mv 'mv -i'
+
+    abbr -a gs 'git status'
+    abbr -a gp 'git push'
+    abbr -a gu 'gitui'
+    abbr -a cm 'chezmoi'
+
+    # use command to bypass the alias
+    # https://github.com/fish-shell/fish-shell/issues/9136#issuecomment-1216988800
+    if command -v trash > /dev/null
+        abbr -a rm 'echo "This is not the command you are looking for."; false'
+    end
+
+    # https://github.com/eza-community/eza
+    if command -v eza > /dev/null
+        abbr -a l 'eza'
+        abbr -a ls 'eza'
+        abbr -a ll 'eza -l'
+        abbr -a lll 'eza -la'
+    else
+        abbr -a l 'ls'
+        abbr -a ll 'ls -l'
+        abbr -a lll 'ls -la'
+    end
+
+    if command -v broot > /dev/null
+        abbr -a l 'br -sdp'
+    end
+
+    # ======================================== function
+    function enable-proxy
+        set -gx https_proxy 'http://localhost:7890'
+        set -gx http_proxy 'http://localhost:7890'
+        set -gx HTTPS_PROXY 'http://localhost:7890'
+        set -gx HTTP_PROXY 'http://localhost:7890'
+        echo proxy on $https_proxy
+    end
+
+    function disable-proxy
+        set -e https_proxy
+        set -e http_proxy
+        set -e HTTPS_PROXY
+        set -e HTTP_PROXY
+        echo proxy off
+    end
+
+    # ctrl+w
+    # https://github.com/fish-shell/fish-shell/issues/3730#issuecomment-317011272
+    function fish_user_key_bindings
+        bind \cw backward-kill-word
+    end
+
+end
+
+# ======================================== plugins
+# ❯ fisher list
+# gazorby/fish-abbreviation-tips
+# nickeb96/puffer-fish
+# jorgebucaran/replay.fish
+# patrickf1/fzf.fish
