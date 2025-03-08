9c47a04dac01122c967d3f0280589eb082f86e6d
jack zhang <jack333zhang@gmail.com>
2025-03-08 11:16:04 +0800

    [fish] x

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -1,9 +1,12 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    fish_add_path ~/.local/bin
+    fish_add_path ~/.local/custom_script
 
     # ======================================== env
     set -U fish_greeting
     set --export EDITOR nvim
+    set --export SSH_AUTH_SOCK ~/.bitwarden-ssh-agent.sock
 
     # ======================================== programming language
     # golang
